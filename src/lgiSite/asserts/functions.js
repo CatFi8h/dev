5e91bd54a3dc678a8564fc6a24dd28aabe6a3ce0
or <jazzmuzzic@gmail.com>
2017-02-28 09:50:02 +0200

    added true backgrounds and logos

--- src/lgiSite/asserts/functions.js
+++ src/lgiSite/asserts/functions.js
@@ -6,7 +6,7 @@ function closeNav() {
     document.getElementById("myNav").style.height = "0%";
 }
 
-function openModal(){
+function openModal() {
     document.getElementById("modal").style.display = "block";
 }
 
@@ -14,7 +14,23 @@ function closeModal() {
     documet.getElementById("modal").style.display = "none";
 }
 
-function currentSlide(n){
+function currentSlide(n) {
 
 
 }
+
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: "smooth"
+});
+
+windows.scrollBy({
+    top:100,
+    left: 0,
+    behavior: "smooth"
+});
+
+document.querySelector('.header').scrollIntoView({
+    behavior: 'smooth'
+});
